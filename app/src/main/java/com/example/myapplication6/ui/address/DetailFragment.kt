8acce3ccd08ab996395b6d3e280cebf55e0bcb3b
htk2021 <htk2021@postech.ac.kr>
2023-07-03 21:37:50 +0900

    first commit

---
+++ app/src/main/java/com/example/myapplication6/ui/address/DetailFragment.kt
@@ -0,0 +1,44 @@
+package com.example.myapplication6.ui.address
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import com.example.myapplication6.databinding.FragmentDetailBinding
+
+class DetailFragment : Fragment() {
+
+    private var _binding: FragmentDetailBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentDetailBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val name = arguments?.getString("name")
+        val age = arguments?.getString("age")
+        val additionalInfo = arguments?.getString("additionalInfo")
+
+        val textName: TextView = binding.textName
+        textName.text = name
+
+        val textAge: TextView = binding.textAge
+        textAge.text = age
+
+        val textAdditionalInfo: TextView = binding.textAdditionalInfo
+        textAdditionalInfo.text = additionalInfo
+
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
